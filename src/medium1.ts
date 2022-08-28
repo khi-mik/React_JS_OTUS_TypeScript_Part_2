6c6372186a406300a9e3e88f5e3cca06026d3434
Mikhail Khizhnyakov <64615735+khi-mik@users.noreply.github.com>
2022-08-28 21:12:42 +0300

    Update medium1.ts

--- src/medium1.ts
+++ src/medium1.ts
@@ -5,39 +5,43 @@
 // Нужно заменить FIXME на правильный тип вычисленный на основе Order
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
+type WithoutInitialAndInWorkOrder = Exclude<
+  Order["state"],
+  "initial" | "inWork"
+>;
+type FIXME = Exclude<Order, { state: WithoutInitialAndInWorkOrder }> | null;
 
 type Order =
   | {
-  state: "initial";
-  sum: number;
-}
+      state: "initial";
+      sum: number;
+    }
   | {
-  state: "inWork";
-  sum: number;
-  workerId: number;
-}
+      state: "inWork";
+      sum: number;
+      workerId: number;
+    }
   | {
-  state: "buyingSupplies";
-  sum: number;
-  workerId: number;
-  suppliesSum: number;
-}
+      state: "buyingSupplies";
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+    }
   | {
-  state: "producing";
-  sum: number;
-  workerId: number;
-  suppliesSum: number;
-  produceEstimate: Date;
-}
+      state: "producing";
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+      produceEstimate: Date;
+    }
   | {
-  state: "fullfilled";
-  sum: number;
-  workerId: number;
-  suppliesSum: number;
-  produceEstimate: Date;
-  fullfillmentDate: Date;
-};
+      state: "fullfilled";
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+      produceEstimate: Date;
+      fullfillmentDate: Date;
+    };
 
 export const filterOnlyInitialAndInWorkOrder = (order: Order): FIXME => {
   if (order.state === "initial" || order.state === "inWork") {
