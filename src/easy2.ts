50be9ecc2a1a50f41dfce77b3eaa9db5646cbd3f
Mikhail Khizhnyakov <64615735+khi-mik@users.noreply.github.com>
2022-08-28 21:10:13 +0300

    Update easy2.ts

--- src/easy2.ts
+++ src/easy2.ts
@@ -3,38 +3,39 @@
 // Нужно заменить FIXME на тип который достанет из Order все возможные состояния (state)
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
+
+type FIXME = Order["state"];
 
 type Order =
   | {
-  state: "initial";
-  sum: number;
-}
+      state: "initial";
+      sum: number;
+    }
   | {
-  state: "inWork";
-  sum: number;
-  workerId: number;
-}
+      state: "inWork";
+      sum: number;
+      workerId: number;
+    }
   | {
-  state: "buyingSupplies";
-  sum: number;
-  workerId: number;
-  suppliesSum: number;
-}
+      state: "buyingSupplies";
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+    }
   | {
-  state: "producing";
-  sum: number;
-  workerId: number;
-  suppliesSum: number;
-  produceEstimate: Date;
-}
+      state: "producing";
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+      produceEstimate: Date;
+    }
   | {
-  state: "fullfilled";
-  sum: number;
-  workerId: number;
-  suppliesSum: number;
-  produceEstimate: Date;
-  fullfillmentDate: Date;
-};
+      state: "fullfilled";
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+      produceEstimate: Date;
+      fullfillmentDate: Date;
+    };
 
 export const getOrderState = (order: Order): FIXME => order.state;
