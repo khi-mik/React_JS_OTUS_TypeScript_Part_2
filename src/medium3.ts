6e8376c9ec27acb72f8edcd311089dd76fa2b349
Mikhail Khizhnyakov <64615735+khi-mik@users.noreply.github.com>
2022-08-28 21:17:48 +0300

    Update medium3.ts

--- src/medium3.ts
+++ src/medium3.ts
@@ -1,18 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
-
 const orderStates = [
   "initial",
   "inWork",
   "buyingSupplies",
   "producing",
   "fullfilled",
-] as const;
+] as const
 
-type OrderState = typeof orderStates[number];
-
+type OrderState = typeof orderStates[number]
+type FIXME = Omit<typeof orderStates[number], "buyingSupplies" | "producing">[]
 // Hint: type guards
 export const getUserOrderStates = (orderStates: OrderState[]): FIXME =>
   orderStates.filter(
     (state) => state !== "buyingSupplies" && state !== "producing"
-  );
+  ) 
